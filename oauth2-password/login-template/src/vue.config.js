03dc4567f528b5fb8df03875ae8e1fae08822311
zhoubiao <biao@zhoubiaodeMacBook-Pro.local>
2020-02-04 15:50:47 +0800

    前端页面

--- oauth2-password/login-template/src/vue.config.js
+++ oauth2-password/login-template/src/vue.config.js
@@ -1,12 +1,1 @@
-// module.exports = {
-//   proxyTable: {
-//     "/api": {
-//       target: "http://localhost:9093", // 要请求的地址
-//       secure: false,
-//       changeOrigin: true,
-//       pathRewrite: {
-//         "^/api": "/"
-//       }
-//     }
-//   }
-// };
+
